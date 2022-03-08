e2d7fcb829e6d445e24907f3f27ca6811264d457
German Gasparotti <ggasparotti@1valet.com>
2022-03-08 11:00:41 -0500

    Typescript version with 1 api

--- packages/backend/src/api/loaders/index.ts
+++ packages/backend/src/api/loaders/index.ts
@@ -2,7 +2,7 @@ import expressLoader from './express';
 import dependencyInjectorLoader from './dependencyInjector';
 import Logger from './logger';
 //We have to import at least all the events once so they can be triggered
-import './events';
+// import './events';
 
 export default async ({ expressApp }) => {
     Logger.info('✌️ DB loaded and connected!');
--- packages/backend/src/api/routes/tamaconsoles.ts
+++ packages/backend/src/api/routes/tamaconsoles.ts
@@ -2,23 +2,22 @@ import { Router, Request, Response, NextFunction } from 'express'
 import { Container } from 'typedi'
 import config from '@/config'
 import { IConsole } from '../interfaces/IConsole'
-import { Logger } from 'winston'
 
 export default (app: Router) => {
 
-    const agendaInstance = Container.get('agendaInstance')
+    // const agendaInstance = Container.get('agendaInstance')
 
     app.post('/consoles',
         async (req: Request, res: Response, next: NextFunction) => {
-            const logger: Logger = Container.get('logger');
-            logger.debug('Calling Sign-Up endpoint with body: %o', req.body);
+            console.log('Calling Sign-Up endpoint with body: %o', req.body);
             try {
                 const consoleBody = req.body as IConsole;
+                console.log('Calling Sign-Up endpoint with body: %o', consoleBody);
                 // const authServiceInstance = Container.get(AuthService);
                 // const { user, token } = await authServiceInstance.SignUp(req.body as IConsole);
                 return res.status(201).json({});
             } catch (e) {
-                logger.error('🔥 error: %o', e);
+                console.log('🔥 error: %o', e);
                 return next(e);
             }
         },
